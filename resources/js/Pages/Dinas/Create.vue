8c17014b586394ab310cef8e70588997f9bcb433
Rifqi Mubarak <45573788+rifqind@users.noreply.github.com>
2024-03-06 16:01:41 +0800

    Update Create.vue

--- resources/js/Pages/Dinas/Create.vue
+++ resources/js/Pages/Dinas/Create.vue
@@ -6,22 +6,27 @@ import Multiselect from '@vueform/multiselect';
 import VueMultiselect from 'vue-multiselect';
 
 const page = usePage()
-defineProps({
-    kabs: Object
-})
+const kabs = page.props.kabs
 defineComponent({
     Multiselect, VueMultiselect
 })
-const value = ref("")
-const options = [
-    { label: "Provinsi", value: 0 },
-    { label: "Kabupaten/Kota", value: 1 },
-    { label: "Kecamatan", value: 2 },
-    { label: "Desa/Kelurahan", value: 3 },
-]
-const optionsLabels = function ({ label }) {
-    return `${label}`
+const tingkatan = ref({
+    value: null,
+    options: [
+        { label: "Provinsi", value: 0 },
+        { label: "Kabupaten/Kota", value: 1 },
+        { label: "Kecamatan", value: 2 },
+        { label: "Desa/Kelurahan", value: 3 },
+    ]
+})
+const kabsUsed = []
+for (const key in kabs) {
+    if (key > 0) kabsUsed.push({ label: kabs[key].label, value: kabs[key].wilayah_fullcode })
 }
+const kabsDrop = ref({
+    value: null,
+    options: kabsUsed
+})
 </script>
 
 <template>
@@ -39,26 +44,32 @@ const optionsLabels = function ({ label }) {
                         <input class="form-control" name="nama" id="nama" placeholder="Isi Nama Produsen Data">
                         <div id="error-nama" class="text-danger mb-3"></div>
                         <div name="wilayah_fullcode" class="d-none">x</div>
-                        <div class="mb-3">
+                        <!-- <div class="mb-3">
                             <label for="tingkat-label">Tingkatan Wilayah</label>
-                            <VueMultiselect v-model="value" :options="options" :custom-label="optionsLabels"
+                            <VueMultiselect v-model="tingkatan" :options="options" :custom-label="optionsLabels"
                                 placeholder="-- Pilih Tingkatan --" label="name" track-by="name">
                             </VueMultiselect>
+                        </div> -->
+                        <div class="mb-3">
+                            <label for="tingkat-label">Tingkatan Wilayah</label>
+                            <Multiselect v-model="tingkatan.value" :options="tingkatan.options"
+                                placeholder="-- Pilih Tingkatan --" />
                         </div>
-                        <div class="mb-3 d-none" id="kabupaten-group">
+                        <div v-if="tingkatan.value > 0" class="mb-3" id="kabupaten-group">
                             <label for="kab-label">Kabupaten</label>
-                            <select name="kab" class="form-control row-select" id="kab-label-select">
+                            <!-- <select name="kab" class="form-control row-select" id="kab-label-select">
                                 <option value="" disabled selected hidden>-- Pilih Kabupaten --</option>
-                                <option v-for="kab in kabs" :key="kab.wilayah_fullcode" :value="kab.wilayah_fullcode">{{
-                                kab.label }}</option>
-                            </select>
+                                <option v-for="kab in kabs" :key="kab.wilayah_fullcode" :value="kab.wilayah_fullcode">{{ kab.label }}</option>
+                            </select> -->
+                            <Multiselect v-model="kabsDrop.value" :options="kabsDrop.options"
+                                placeholder="-- Pilih Kabupaten --" :searchable="true" />
                         </div>
-                        <div class="mb-3 d-none" id="kecamatan-group">
+                        <div v-if="tingkatan.value > 1" class="mb-3" id="kecamatan-group">
                             <label for="kec-label">Kecamatan</label>
                             <select name="kec" class="form-control row-select" id="kec-label-select">
                             </select>
                         </div>
-                        <div class="mb-3 d-none" id="desa-group">
+                        <div v-if="tingkatan.value > 2" class="mb-3" id="desa-group">
                             <label for="desa-label">Desa</label>
                             <select name="desa" class="form-control row-select" id="desa-label-select">
                             </select>
@@ -74,5 +85,4 @@ const optionsLabels = function ({ label }) {
 </template>
 
 <style src="@vueform/multiselect/themes/default.css"></style>
-
-<style src="vue-multiselect/dist/vue-multiselect.css"></style>
+<!-- <style src="vue-multiselect/dist/vue-multiselect.css"></style> -->
