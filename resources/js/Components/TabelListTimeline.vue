ab2be8fe118022c3b9a85b77fa4d58a0f2060a34
Rifqi Mubarak <45573788+rifqind@users.noreply.github.com>
2024-05-28 13:12:08 +0800

    v3 infinite loading

--- resources/js/Components/TabelListTimeline.vue
+++ resources/js/Components/TabelListTimeline.vue
@@ -1,5 +1,9 @@
 <script setup>
-import { useForm, Link } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
+import InfiniteLoading from 'v3-infinite-loading'
+import "v3-infinite-loading/lib/style.css"
+import { ref, onMounted } from 'vue'
+
 const props = defineProps({
     countTabels: {
         type: Number,
@@ -10,10 +14,21 @@ const props = defineProps({
         required: true,
     }
 })
-const form = useForm({
-    id: null,
-    year: null,
+const displayedData = ref([])
+const loadInitialData = () => {
+    displayedData.value = props.data.slice(0, 20)
+}
+const loadMoreData = (state) => {
+    let nextData = props.data.slice(displayedData.value.length, displayedData.value.length + 20)
+    if (nextData.length) {
+        displayedData.value = displayedData.value.concat(nextData)
+        state.loaded()
+    } else state.complete()
+}
+onMounted(() => {
+    loadInitialData()
 })
+const infiniteData = +new Date()
 </script>
 <template>
     <div class="card-header py-3">
@@ -21,10 +36,10 @@ const form = useForm({
             Menampilkan : {{ countTabels }} tabel</h6>
     </div>
     <div class="card-body" id="list-tabel-card">
-        <div v-for="(node, index) in data" :key="index" class="d-flex flex-column mb-2 ml-3">
+        <div v-for="(node, index) in displayedData" :key="index" class="d-flex flex-column mb-2 ml-3">
             <Link :href="`/show?id=${node.id_statustables}&year=${node.tahun}`" class="text-red behave-a">
-                <span class="badge badge-pill badge-danger">{{ node.nomor }}</span> -
-                {{ node.label }}, Tahun {{ node.tahun }}
+            <span class="badge badge-pill badge-danger">{{ node.nomor }}</span> -
+            {{ node.label }}, Tahun {{ node.tahun }}
             </Link>
             <small class="lead smalltext-homepage">{{ node.nama_dinas }} |
                 {{ node.nama_regions }}</small>
@@ -32,6 +47,9 @@ const form = useForm({
                 {{ node.nama_subjects }}</small>
             <small class="lead smalltext-homepage">Terakhir diupdate :
                 {{ node.status_updated }}</small>
+        </div>
+        <div class="text-center">
+            <InfiniteLoading @infinite="loadMoreData" :identifier="infiniteData" />
         </div>
     </div>
 </template>
